a10d86b36843506b035340a025551df2d90590eb
talonendm <talonen.dm@gmail.com>
2020-12-01 20:44:55 +0200

    double click test

--- sketch.js
+++ sketch.js
@@ -4,7 +4,10 @@ var value = 0;
 var endclicks = 0;
 var doublec = 0;
 var movec = 0;
-
+var ptimer;
+var clicked=false, clickTimeout=300;
+var clicks2 = 0;
+var doublec2 = 0;
 function setup() { 
   createCanvas(windowWidth, windowHeight);
 	colorMode(HSB,360,100,100);
@@ -22,9 +25,9 @@ function draw() {
 	textAlign(CENTER,CENTER);
 	textFont('Avenir');
 	let permin = round(clicks * 600 / timerValue);
-	text('click: ' + clicks + "TIME" + nfc(timerValue / 10,1) + "\nperMin:" + permin, width*0.5,height*0.5+2);
+	text('click: ' + clicks + " OR " + clicks2 + "TIME" + nfc(timerValue / 10,1) + "\nperMin:" + permin, width*0.5,height*0.5+2);
 	text('touch move: ' + movec, width*0.5,height*0.35+2);
-	text('double: ' + doublec, width*0.5,height*0.65+2);
+	text('double: ' + doublec + " or " + doublec2, width*0.5,height*0.65+2);
 	text('end: ' + endclicks, width*0.5,height*0.8+2);
 }
 
@@ -36,8 +39,42 @@ function touchStarted () {
     fullscreen(true);
   }
   
-  clicks = clicks + 1;
+  
   value = 0;
+  
+  
+  
+  if (ptimer == timerValue) {
+	  // double
+	  doublec = doublec + 1; # // within selected time 0.1sec
+  } else {
+	  clicks = clicks + 1;
+  }
+  // https://stackoverflow.com/questions/51144762/p5-js-mousepressed-works-but-doublepressed-doesnot
+  ptimer = timerValue;
+  
+  
+  if(!clicked){ //# https://stackoverflow.com/questions/51144762/p5-js-mousepressed-works-but-doublepressed-doesnot
+    clicked=true;
+    setTimeout(function(){
+      if(clicked){
+        console.log("single click");
+        clicked=false;
+        //single ClickStuff
+		clicks2 = clicks2 + 1;
+      }
+    },clickTimeout);
+  }else{
+    clicked=false;
+    console.log("double click");
+    //double click Stuff
+	doublec2 = doublec2 + 1;
+  }
+  
+  
+  
+  
+  
 }
 
 function touchEnded() { 
@@ -50,9 +87,9 @@ function touchMoved() {
 }
 
 // this function fires with any double click anywhere
-function doubleClicked() {
-	doublec = doublec + 1;
-}
+//function doubleClicked() {
+//	doublec = doublec + 1;
+//}
 
 
 
